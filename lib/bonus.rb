da5f120840530fa19c102a813096def93c82b3cf
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 04:00:52 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,7 +23,7 @@ def bonus
   #code your solution here:
 
   
-
- epic_tragedy=[:capulet][:heroine][:status]= "dead"
- epic_tragedy=[:montague][:hero][:status]= "dead"
 end
+ 
+endepic_tragedy=[:capulet][:heroine][:status]= "dead"
+ epic_tragedy=[:montague][:hero][:status]= "dead"
