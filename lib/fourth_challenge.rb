e0ccf68bedd20961f8f9f6055b32991e417133c6
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 03:45:53 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -19,7 +19,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-       {name: "Steven", age: "30", attitude: "hot-headed"}
+       {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Steven", age: "30", attitude: "confused"}
       ]
    }
 }
