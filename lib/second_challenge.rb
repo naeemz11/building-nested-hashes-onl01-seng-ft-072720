438d044ef678dfdcf4d8ce4dfd2791202c8914e7
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 03:26:59 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,12 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-     
+:patriarch => {}
+:matriarch => {}
+:hero => {}
+:hero_friends => []
       }, 
+      
     :capulet => {
       #your key/value pairs here!
       }}
