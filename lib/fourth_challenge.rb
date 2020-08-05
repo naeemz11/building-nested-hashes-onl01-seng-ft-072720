22695628789aa8870e8067d4ab5239539edccef2
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 03:43:56 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,11 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
       {
-        name: "Benvolio", age: "17", attitude: 
+        name: "Benvolio", age: "17", attitude: "worried"
+      },
+      
+      {name: "Benvolio", age: "17", attitude: "worried"
+        
       }
       ]
    }, 
